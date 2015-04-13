ed0c9de7b03ab30390fbc0940a83f05a01682afb
Bart <bart.okkels@hva.nl>
2015-04-13 11:54:51 +0200

    begin van de app

---
+++ forgetMeNot/src/com/example/forgetmenot/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.forgetmenot;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void addThing(View view) {
+        Intent intent = new Intent(this, AddThing.class);
+        
+        startActivity(intent);
+    }
+    
+    public void about(View view) {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
+    }
+}
