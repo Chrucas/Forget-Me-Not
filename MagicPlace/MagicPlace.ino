71fc3d3be5a445c5b29f8f22ac54f0615062d5e8
Bart <bart.okkels@hva.nl>
2015-05-01 08:32:10 +0200

    kommentaar veranderd

--- MagicPlace/MagicPlace.ino
+++ MagicPlace/MagicPlace.ino
@@ -1,26 +1,27 @@
-int start = 7; // om programma te starten
-int stopButton = 2; // om de motor te stoppen
-int relay = 4; // om de motor te laten draaien HIGH is uit LOW is aan
-//static int x ;
+int start = 7; // Button to start the program
+int stopButton = 2; // Button to stop the program when it made one round
+int relay = 4; // relay to control the motor 
+
 
 void setup() {
+  // sets the data rate in bits per second for serial data transmission
+  Serial.begin(9600); 
+  pinMode(start, INPUT); // set start to input
+  pinMode(stopButton, INPUT); // set stopButton to input
+  pinMode(relay, OUTPUT); // set relay to output
+  digitalWrite(relay, HIGH); // put the motor off
   
-  Serial.begin(9600);
-  pinMode(start, INPUT);
-  pinMode(stopButton, INPUT);
-  pinMode(relay, OUTPUT);
-  digitalWrite(relay, HIGH);
 }
 
 void loop() {
   
-  int startState= digitalRead(start);
-  int buttonState = digitalRead(stopButton);
+  int startState= digitalRead(start); // read the state of start
+  int buttonState = digitalRead(stopButton); // read the state of stopbutton
   
   Serial.print("start state ");
-  Serial.println(startState);
+  Serial.println(startState); // print the startstate in the seriele monitor
   Serial.print("button state ");
-  Serial.println(buttonState);
+  Serial.println(buttonState); // print the buttonstate in the seriele monitor
   
   if(startState == HIGH){
     startMotor();
@@ -30,11 +31,11 @@ void loop() {
 }
 
 void startMotor(){
-  digitalWrite(relay, LOW);
+  digitalWrite(relay, LOW); // start the motor
 }
 
 void stopMotor(){
-  digitalWrite(relay, HIGH);
+  digitalWrite(relay, HIGH); // stop the motor
 }
 
 
